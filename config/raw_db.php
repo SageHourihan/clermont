8371ff7e5b7aab19c22aded6b35f481bfb9abc87
SageHourihan <samiho97@gmail.com>
2025-02-16 20:51:42 +0000

    added try catch

--- config/raw_db.php
+++ config/raw_db.php
@@ -6,7 +6,7 @@ use MongoDB\Client;
 use MongoDB\Driver\ServerApi;
 
 function writeData(){
-
+    $error = null;
 
     // Load environment variables from .env file
     $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
@@ -24,13 +24,6 @@ function writeData(){
     $client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
     $collection = $client->clermont->raw_data   ;
 
-// try {
-//     // Send a ping to confirm a successful connection
-//     $client->selectDatabase('clermont')->command(['ping' => 1]);
-//     echo "Pinged your deployment. You successfully connected to MongoDB!\n";
-// } catch (Exception $e) {
-//     printf($e->getMessage());
-// }
     // Read the file line by line and decode each JSON object
     $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     $jsonArray = array_map('json_decode', $lines);
@@ -47,27 +40,25 @@ function writeData(){
         $longitude = $data['Message']['PositionReport']['Longitude'] ?? null;
         $sog = $data['Message']['PositionReport']['Sog'] ?? null;
 
-        // Insert data into MongoDB
-        $insertOneResult = $collection->insertOne([
-            'messageType' => $messageType,
-            'mmsi' => $mmsi,
-            'shipName' => $shipName,
-            'latitude' => $latitude,
-            'longitude' => $longitude,
-            'sog' => $sog,
-        ]);
+        try {
+            // Insert data into MongoDB
+            $insertOneResult = $collection->insertOne([
+                'messageType' => $messageType,
+                'mmsi' => $mmsi,
+                'shipName' => $shipName,
+                'latitude' => $latitude,
+                'longitude' => $longitude,
+                'sog' => $sog,
+            ]);
+        } catch (Exception $e) {
+            $error = $e->getMessage();
+        }
 
-        printf("Inserted %d document(s)\n", $insertOneResult->getInsertedCount());
-        var_dump($insertOneResult->getInsertedId());
-
-        // Output extracted values
-        // echo "Message Type: $messageType\n";
-        // echo "MMSI: $mmsi\n";
-        // echo "Ship Name: $shipName\n";
-        // echo "Latitude: $latitude\n";
-        // echo "Longitude: $longitude\n";
-        // echo "Speed Over Ground (SOG): $sog\n";
-        // echo "---------------------------------\n";
+        if(!$error){
+            echo "Inserted %d document(s)\n", $insertOneResult->getInsertedCount();
+        }else {
+            echo(json_encode($error));
+        }
     }
 }
 
