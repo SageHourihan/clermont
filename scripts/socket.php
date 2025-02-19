2fbc5292f912591a2f983ac9515cff3981a48c83
SageHourihan <samiho97@gmail.com>
2025-02-19 03:16:10 +0000

    storing data in memcache

--- scripts/socket.php
+++ scripts/socket.php
@@ -14,6 +14,10 @@ $apiKey = $_ENV['AISSTREAM_API_KEY'];
 // Connect to the WebSocket server
 $client = new Client("wss://stream.aisstream.io/v0/stream");
 
+// Connect to Memcached
+$memcache = new Memcached();
+$memcache->addServer('127.0.0.1', 11211);
+
 // Prepare the message
 $message = json_encode([
    "APIKey" => $apiKey,  // API Key from environment
@@ -30,7 +34,22 @@ while (true) {
     try {
         // Receive a message from the WebSocket server
         $incomingMessage = $client->receive();
-        echo "Received message: $incomingMessage\n";
+        // echo "Received message: $incomingMessage\n";
+
+        // Unescape the JSON string (convert escaped quotes to normal quotes)
+        $unescapedMessage = stripslashes($incomingMessage);
+        // Decode the unescaped JSON string into an associative array
+        $data = json_decode($unescapedMessage, true);
+
+        if(isset($data['MetaData']['MMSI'])){
+            $mmsi = $data['MetaData']['MMSI'];
+
+            $memcache->set("ship:$mmsi", $incomingMessage, 3600);
+            echo 'stored data in memcache';
+        }else{
+            echo 'no mmsi in messag';
+        }
+
         file_put_contents('/var/www/html/clermont/data/data.json', json_encode($incomingMessage) . "\n", FILE_APPEND);
     } catch (Exception $e) {
         echo "Error: " . $e->getMessage() . "\n";
