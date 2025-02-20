a4fe2c67847503fd4e0cac98595100066e2c3759
SageHourihan <samiho97@gmail.com>
2025-02-20 20:02:42 +0000

    updated getData.php

--- src/api/getData.php
+++ src/api/getData.php
@@ -1,11 +1,21 @@
 <?php
 
-$memcache = new Memcached();
-$memcache->addServer('127.0.0.1', 11211);
+// Include autoload and constants for autoloading the service classes and dotenv
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../config/constants.php';  // Load the constant
 
-function getData($memcache) {
+use Services\CacheService;
+
+// Load environment variables from .env file
+$dotenv = Dotenv\Dotenv::createImmutable(PROJECT_ROOT);
+$dotenv->load();
+
+// Create an instance of CacheService
+$cacheService = new CacheService();
+
+function getData($cacheService) {
     // Get the list of active ships (an array of MMSIs)
-    $activeShips = $memcache->get("ships:active");
+    $activeShips = $cacheService->getActiveShips();
 
     // If there are no active ships, return an empty array
     if ($activeShips === false || !is_array($activeShips)) {
@@ -16,7 +26,7 @@ function getData($memcache) {
 
     // Retrieve each ship's data from Memcached
     foreach ($activeShips as $mmsi) {
-        $shipInfo = $memcache->get("ship:$mmsi");
+        $shipInfo = $cacheService->getShipData($mmsi);
 
         if ($shipInfo !== false) {
             $shipData[] = json_decode($shipInfo, true);
@@ -25,11 +35,8 @@ function getData($memcache) {
 
     // Return as JSON
     header('Content-Type: application/json');
-    // echo json_encode($shipData);
     echo json_encode($shipData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 }
 
 // Call the function
-getData($memcache);
-
-
+getData($cacheService);
